15dbe6d4113a21f77ff2157ac038817548933d96
AtanasiusSuryaGunadharma <220711667@students.uajy.ac.id>
2024-09-12 11:57:33 +0700

    Update st_diabetes_exp.py

--- st_diabetes_exp.py
+++ st_diabetes_exp.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle
 import os
 #load model
-model_directory = r'https://drive.google.com/drive/folders/1z6UxHnF-DrA9MMazPKwySROv7rV5_VQ8?usp=sharing' ##diisi dengan path folder dimana file dataset berada
+model_directory = r'D:\SURYA\UAJY\Semester 5\Asdos Machine Learning\Modul Praktik\TesColabDiabetes' ##diisi dengan path folder dimana file dataset berada
 
 model_path = os.path.join(model_directory, r'D:\SURYA\UAJY\Semester 5\Asdos Machine Learning\Modul Praktik\TesColabDiabetes\st_diabetes_exp.py') 
 # Periksa apakah file ada di direktori yang ditentukan
